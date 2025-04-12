4e267d6f84f9033bcf0c3af6445a56a952a9cc57
SCPLEGION <jacek.adamiec002.op.pl@gmail.com>
2025-04-12 12:12:16 +0200

    woking

--- src/modules/redirector.js
+++ src/modules/redirector.js
@@ -3,13 +3,10 @@ import { handleInteraction } from "./interaction.js";
 
 export function redirect(msg) {
     if (msg.author.bot) return;
-
     if (msg.channelId === '1354178493784654054') {
         createTicket(msg, 'Pomoc');
     } else if (msg.channelId === '1354178654753521674') {
         createTicket(msg, 'Pytanie');
-    } else if (msg.channelId === '1354178702782763118') {
-        createTicket(msg, 'Propozycja');
     }
 }
 
